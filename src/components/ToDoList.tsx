b07e6e9b23d0aa857aa2030859864b39c5755278
JHKIMS <wjdgns5131@gmail.com>
2023-01-18 14:39:33 +0900

    Todo: Selector을 사용해서 Category분류2

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,6 @@
 import { useRecoilState, useRecoilValue } from "recoil";
 import styled from "styled-components";
-import { toDoSelector, toDoState } from "../atoms";
+import { categoryState, toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -11,16 +11,29 @@ const Ul = styled.ul`
   color: white;
 `;
 function ToDoList() {
-  const [todo, doing, done] = useRecoilValue(toDoSelector);
+  const todos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
 
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
+  console.log(category);
   /*   const value = useRecoilValue(toDoState); === todos
     const modFn = useSetRecoilState(toDoState);  === setTodos(CreateToDo.tsx)*/
   return (
     <div>
       <Title>What's Your Plan?</Title>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TODO">TODO</option>
+        <option value="DOING">DOING</option>
+        <option value="DONE">DONE</option>
+      </select>
       <CreateToDo />
-      <h2>To do</h2>
+      {todos?.map((todo) => (
+        <ToDo key={todo.id} {...todo} />
+      ))}
+      {/* <h2>To do</h2>
       <Ul>
         {todo.map((todo) => (
             <ToDo key={todo.id} {...todo}/>
@@ -43,7 +56,7 @@ function ToDoList() {
         // 위의 코드와 동일하다. <ToDo text={todo.text} category={todo.category} id={todo.id} />
         ))}
       </Ul>
-      <hr />
+      <hr /> */}
     </div>
   );
 }
